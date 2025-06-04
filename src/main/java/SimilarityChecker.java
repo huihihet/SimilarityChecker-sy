4d8f1d9c8c6b192d92bbb1f0c99097ef6c6f4147
Jinchan <zxcvb2023@naver.com>
2025-06-04 16:55:20 +0900

    [feature]1

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,2 +1,13 @@
 public class SimilarityChecker {
+    public int getLengthCheck(String input1, String input2) {
+        if (input1.length() == input2.length())
+            return 60;
+        else if (input1.length() < input2.length())
+            return 0;
+        else {
+            int gap = input1.length() - input2.length();
+            double rate = (double) gap / input2.length();
+            return (int) ((1 - rate) * 60);
+        }
+    }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,5 +1,35 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityCheckerTest {
 
+    private SimilarityChecker similarityChecker;
+
+    @BeforeEach
+    void setUp() {
+        similarityChecker = new SimilarityChecker();
+    }
+
+    @Test
+    void lengthCheckSameLength() {
+        checkSimilarity(60, "ASD", "DSA");
+    }
+
+    @Test
+    void lengthCheckInput2Longer() {
+        checkSimilarity(0, "A", "BB");
+    }
+
+
+    @Test
+    void lengthCheckInput1Longer1() {
+        checkSimilarity(20, "AAABB", "BAA");
+    }
+
+    private void checkSimilarity(int expected, String input1, String input2) {
+        int actual = similarityChecker.getLengthCheck(input1, input2);
+        assertEquals(expected, actual);
+    }
 }
