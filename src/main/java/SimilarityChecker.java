7eb2bf183c967100c397fdeee8c78b5d0d14f8b4
Jinchan <zxcvb2023@naver.com>
2025-06-04 17:03:57 +0900

    [feature]2

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -2,11 +2,13 @@ public class SimilarityChecker {
     public int getLengthCheck(String input1, String input2) {
         if (input1.length() == input2.length())
             return 60;
-        else if (input1.length() < input2.length())
-            return 0;
         else {
-            int gap = input1.length() - input2.length();
-            double rate = (double) gap / input2.length();
+            int longer = Math.max(input1.length(), input2.length());
+            int shorter = Math.min(input1.length(), input2.length());
+            if (shorter * 2 <= longer) return 0;
+
+            int gap = longer - shorter;
+            double rate = (double) gap / shorter;
             return (int) ((1 - rate) * 60);
         }
     }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -14,21 +14,26 @@ class SimilarityCheckerTest {
 
     @Test
     void lengthCheckSameLength() {
-        checkSimilarity(60, "ASD", "DSA");
+        checkLengthSimilarity(60, "ASD", "DSA");
     }
 
     @Test
-    void lengthCheckInput2Longer() {
-        checkSimilarity(0, "A", "BB");
+    void lengthCheck1() {
+        checkLengthSimilarity(0, "A", "BB");
     }
-
 
     @Test
-    void lengthCheckInput1Longer1() {
-        checkSimilarity(20, "AAABB", "BAA");
+    void lengthCheck2() {
+        checkLengthSimilarity(20, "AAABB", "BAA");
     }
 
-    private void checkSimilarity(int expected, String input1, String input2) {
+    @Test
+    void lengthCheck3() {
+        checkLengthSimilarity(30, "AA", "AAE");
+    }
+
+
+    private void checkLengthSimilarity(int expected, String input1, String input2) {
         int actual = similarityChecker.getLengthCheck(input1, input2);
         assertEquals(expected, actual);
     }
